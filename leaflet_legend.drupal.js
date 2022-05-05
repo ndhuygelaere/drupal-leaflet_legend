4981edcde365d01d604f5700e4fb4e38b7d721d0
Root webnuxdev <root@webnuxdev.rnde.tm.fr>
2022-05-05 14:24:30 +0200

    add primitive for markers management

--- leaflet_legend.drupal.js
+++ leaflet_legend.drupal.js
@@ -4,9 +4,24 @@
 
 (function($, Drupal, drupalSettings) {
     $(document).on('leafletMapInit', function(e, settings, lMap, mapid) {
-        
-        //console.log(mapid);
         items = [];
+        markers_type = [];
+        //Manage markers layer 
+        //fix me manage case when the marker is not an image
+        //console.log(settings);
+        //console.log(lMap);
+        //console.log(mapid);
+        $.each(Drupal.Leaflet[mapid].markers, function(i, val){
+            if(markers_type.indexOf(val._icon.src) === -1){
+                markers_type.push(val._icon.src);
+                items.push({
+                    label: "Marker" + markers_type.length,
+                    type: "image",
+                    url: val._icon.src
+                });
+            }
+        });
+        //Manage WMS Layers
         $.each(settings.layers, function(i, val){
             if(typeof val.legend !==  "undefined" && val.legend){
                 items.push(val.legend);
